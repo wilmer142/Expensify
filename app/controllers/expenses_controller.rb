8e7ceb8268ae56ad5e0d863d96d008cda3e9f6bc
wilmer142 <wilmercollege@gmail.com>
2018-04-25 19:53:59 -0500

    Create Expense action with modal

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,22 @@
 class ExpensesController < ApplicationController
+
   def index
     @tab = :expenses
     @expenses = Expense.all
   end
+
+  def new
+  	@expense = Expense.new
+  end
+
+  def create
+  	@expense = Expense.create(expense_params)
+  end
+
+  private
+
+  	def expense_params
+  		params.require(:expense).permit(:type, :date, :concept, :category, :amount)
+  	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "dashboard#index"
 
-  resources :expenses, only: [:index]
+  resources :expenses, only: [:index, :new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
